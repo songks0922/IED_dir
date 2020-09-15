b79aefffd2f17bbe098c291594ad907beb5c571a
songks0922 <skj0922@kookmin.ac.kr>
2020-09-15 17:55:32 +0900

    second

--- 04_example_3/04_example_3.ino
+++ 04_example_3/04_example_3.ino
@@ -6,7 +6,7 @@ void setup() {
   Serial.begin(115200); // Initialize serial port
   while (!Serial) {
     ; // wait for serial port to connect.
-  }
+ }
   Serial.println("Hello World!");
   count = toggle = 0;
   digitalWrite(PIN_LED, toggle); // turn off LED.
@@ -14,12 +14,11 @@ void setup() {
 
 void loop() {
   Serial.println(++count);
-  toggle = toggle_state(count); //toggle LED value.
+  toggle = toggle_state(toggle); //toggle LED value.
   digitalWrite(PIN_LED, toggle); // update LED status.
-  
   delay(1000); // wait for 1,000 milliseconds
 }
 
 int toggle_state(int toggle) {
-  return toggle % 2;
+  return count % 2;
 }
